ff6a68d9832c14d8040c14de6cb1d0d254c60da5
Samuel Odigie <samodigie98@gmail.com>
2024-11-18 03:46:28 +0000

    small clean up

--- src/components/starshipCard.jsx
+++ src/components/starshipCard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { starShips } from "../starwarzApi";
-import StarshipCardItem from "./StarshipCardItem";
+import StarshipCardItem from "./starshipCardItem";
 import { Loading } from "@carbon/react";
 
 const StarshipCard = () => {
